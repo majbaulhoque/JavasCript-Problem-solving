5f9c1914f2a5bc145df59f085c07935a2484c2ac
Majba UL Hoque <majba9399@gmail.com>
2024-09-24 17:08:35 +0600

    sum of total number

---
+++ sumOfTotalSum.js
@@ -0,0 +1,13 @@
+const sum = (params1, params2) =>{
+    return params1 + params2;
+}
+
+const totalSum = (...params1) =>{
+    let total = 0
+    params1.forEach(eachValue => {
+        total += eachValue
+    })
+    return total
+}
+
+console.log(totalSum(2, 5, 8))
